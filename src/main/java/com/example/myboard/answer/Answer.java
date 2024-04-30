0e8705cb2d102f26d1e0f7b55d055edd08adb6fd
justlikeryu <justlikeryuu@gmail.com>
2024-04-30 17:53:11 +0900

    Feat: 질문 및 답변 엔터티/레포지토리 작성

---
+++ src/main/java/com/example/myboard/answer/Answer.java
@@ -0,0 +1,25 @@
+package com.example.myboard.answer;
+
+import com.example.myboard.question.Question;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne //질문 하나에 답변은 여러개
+    private Question question;
+}
---
+++ src/main/java/com/example/myboard/question/Question.java
@@ -0,0 +1,29 @@
+package com.example.myboard.question;
+
+import com.example.myboard.answer.Answer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //해당 속성만 번호가 1씩 증가
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT") //컬럼데이터의 유형 정의, 글자 수 제한 X
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) //질문 하나에 답변은 여러개
+    private List<Answer> answerList;
+}
