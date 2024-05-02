68d6592b1048021deec1f5d42633a4b0adb2bdac
justlikeryu <justlikeryuu@gmail.com>
2024-05-02 15:18:59 +0900

    Feat: 개별 질문 조회

--- src/main/java/com/example/myboard/question/QuestionController.java
+++ src/main/java/com/example/myboard/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,5 +21,13 @@ public class QuestionController {
         model.addAttribute("list", questionList);
 
         return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+
+        return "question_detail";
     }
 }
