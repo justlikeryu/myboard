9a315d0418c961b365c2db0e6852e51c06c22a6e
justlikeryu <justlikeryuu@gmail.com>
2024-05-02 22:36:12 +0900

    Feat: 게시물 최신순으로 조회하기

--- src/main/java/com/example/myboard/question/QuestionService.java
+++ src/main/java/com/example/myboard/question/QuestionService.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -16,7 +19,11 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
 
     public Page<Question> getList(int page) {//페이지 번호를 입력 받으면 해당 페이지의 객체 리턴
-        Pageable pageable = PageRequest.of(page, 10);//page: 조회할 페이지 번호, 10: 한 페이지에 보여줄 게시물 개수
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));//정렬 조건 추가
+
+        //최신순으로 조회하기 위해 Sort 객체 전달
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));//page: 조회할 페이지 번호, 10: 한 페이지에 보여줄 게시물 개수
 
         return this.questionRepository.findAll(pageable);
     }
