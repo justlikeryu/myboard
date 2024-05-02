f8505e2544dc8233d278784f3c95274d48eefc7e
justlikeryu <justlikeryuu@gmail.com>
2024-05-02 15:09:59 +0900

    Feat: 전체 질문 조회 컨트롤러

---
+++ src/main/java/com/example/myboard/question/QuestionController.java
@@ -0,0 +1,24 @@
+package com.example.myboard.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/question")
+@RequiredArgsConstructor //questionRepository 객체 주입
+@Controller
+public class QuestionController {
+    private final QuestionService questionService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("list", questionList);
+
+        return "question_list";
+    }
+}
