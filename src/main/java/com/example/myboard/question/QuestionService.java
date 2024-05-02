a4357224d7476c21c3086bc2fac54f23822db045
justlikeryu <justlikeryuu@gmail.com>
2024-05-02 19:23:51 +0900

    Feat: 질문 등록하기

--- src/main/java/com/example/myboard/question/QuestionService.java
+++ src/main/java/com/example/myboard/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.example.myboard.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,14 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("Not Found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+
+        this.questionRepository.save(question);
     }
 }
