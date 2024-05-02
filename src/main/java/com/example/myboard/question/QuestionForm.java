d3576e9afb17b1bf547a9d946d8baa70b17cbd04
justlikeryu <justlikeryuu@gmail.com>
2024-05-02 20:52:06 +0900

    Feat: QuestionForm 내용 수정

--- src/main/java/com/example/myboard/question/QuestionForm.java
+++ src/main/java/com/example/myboard/question/QuestionForm.java
@@ -1,2 +1,18 @@
-package com.example.myboard.question;public class QuestionForm {
+package com.example.myboard.question;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionForm {
+    //todo 아무 것도 입력하지 않으면 @NotEmpty에 의해 등록이 되지 않아야 하는데 등록이 되는 현상...
+    @NotEmpty(message = "제목을 입력해 주세요.")//null, 빈 문자열 비허용
+    @Size(max=200)//최대 길이는 200바이트
+    private String subject;
+
+    @NotEmpty(message = "내용을 입력해 주세요.")
+    private String content;
 }
