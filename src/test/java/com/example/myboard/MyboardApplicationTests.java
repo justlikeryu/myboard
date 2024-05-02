c1b1015146121f6d77dc8f52b03256c3f31bdc3b
justlikeryu <justlikeryuu@gmail.com>
2024-05-02 21:56:21 +0900

    Test: 테스트 데이터 생성하는 테스트 케이스

--- src/test/java/com/example/myboard/MyboardApplicationTests.java
+++ src/test/java/com/example/myboard/MyboardApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.myboard.answer.Answer;
 import com.example.myboard.answer.AnswerRepository;
 import com.example.myboard.question.Question;
 import com.example.myboard.question.QuestionRepository;
+import com.example.myboard.question.QuestionService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -74,14 +75,14 @@ class MyboardApplicationTests {
 
     @Test
     @DisplayName("findBySubjectAndContet: 제목과 내용으로 질문 조회")
-    void selectQuestionBySubjectAndContent(){
+    void selectQuestionBySubjectAndContent() {
         Question question = this.questionRepository.findBySubjectAndContent("제목1", "내용1");
         assertEquals(1, question.getId());
     }
 
     @Test
     @DisplayName("findBySubjectLike: 특정 문자열로 질문 조회")
-    void selectBySubjectLike(){
+    void selectBySubjectLike() {
         List<Question> questions = this.questionRepository.findBySubjectLike("제목%");
         Question question = questions.get(0);
         assertEquals("제목1", question.getSubject());
@@ -89,7 +90,7 @@ class MyboardApplicationTests {
 
     @Test
     @DisplayName("editQuestion: 질문 수정하기")
-    void updateQuestion(){
+    void updateQuestion() {
         Optional<Question> question = this.questionRepository.findById(1);
         assertTrue(question.isPresent()); //값이 참인지 확인
 
@@ -100,7 +101,7 @@ class MyboardApplicationTests {
 
     @Test
     @DisplayName("deleteQuestion: 질문 삭제하기")
-    void deleteQuestion(){
+    void deleteQuestion() {
         Optional<Question> question = this.questionRepository.findById(1);
         assertTrue(question.isPresent());
 
@@ -111,7 +112,7 @@ class MyboardApplicationTests {
 
     @Test
     @DisplayName("addAnswer: 답변하기")
-    void addAnswer(){
+    void addAnswer() {
         Optional<Question> question = this.questionRepository.findById(2);
         assertTrue(question.isPresent());
         Question q = question.get();
@@ -125,7 +126,7 @@ class MyboardApplicationTests {
 
     @Test
     @DisplayName("findAnswer: 답변 조회")
-    void selectAnswer(){
+    void selectAnswer() {
         Optional<Answer> answer = this.answerRepository.findById(1);
         assertTrue(answer.isPresent());
 
@@ -136,7 +137,7 @@ class MyboardApplicationTests {
     @Test
     @Transactional
     @DisplayName("findAnswerByQuestion: 질문으로 답변 조회")
-    void selectByAnswer(){
+    void selectByAnswer() {
         Optional<Question> question = this.questionRepository.findById(2);
         assertTrue(question.isPresent());
         Question q = question.get();
@@ -145,5 +146,19 @@ class MyboardApplicationTests {
 
         assertEquals(1, answerList.size());
         assertEquals("답변2", answerList.get(0).getContent());
+    }
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    @DisplayName("createTestCase: 테스트 케이스 추가")
+    void createTestCase() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터:[%03d]", i);
+            String content = "내용무";
+
+            this.questionService.create(subject, content);
+        }
     }
 }
