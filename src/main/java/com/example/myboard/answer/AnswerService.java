cb3c22d4d3d30ac56757b547fb6628ef74bf60fe
justlikeryu <justlikeryuu@gmail.com>
2024-05-02 18:27:25 +0900

    Feat: 답변 등록 및 조회

---
+++ src/main/java/com/example/myboard/answer/AnswerService.java
@@ -0,0 +1,33 @@
+package com.example.myboard.answer;
+
+import com.example.myboard.DataNotFoundException;
+import com.example.myboard.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+}
