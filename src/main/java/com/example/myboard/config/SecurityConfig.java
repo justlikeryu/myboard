257b0428abaad9caa6a0708036aa7f94d51590c8
justlikeryu <justlikeryuu@gmail.com>
2024-05-04 23:28:31 +0900

    Feat: 스프링 시큐리티 설정

---
+++ src/main/java/com/example/myboard/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.myboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration//스프링의 환경 설정 파일임을 의미
+@EnableWebSecurity//모든 요청 URL이 스프링 시큐리티의 제어를 받도록 함
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                .headers((headers)->headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .build();
+    }
+}
