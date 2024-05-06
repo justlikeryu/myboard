532409535d8894474afd1eb3b6e2d83849019278
justlikeryu <justlikeryuu@gmail.com>
2024-05-06 23:12:03 +0900

    Feat: @query를 이용한 검색 기능 구현

--- src/main/java/com/example/myboard/question/QuestionRepository.java
+++ src/main/java/com/example/myboard/question/QuestionRepository.java
@@ -1,9 +1,11 @@
 package com.example.myboard.question;
 
-import org.springframework.data.domain.Page;//페이징을 위한 클래스
-import org.springframework.data.domain.Pageable;//페이징을 처리하는 인터페이스
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,4 +21,18 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Page<Question> findAll(Pageable pageable);
 
     Page<Question> findAll(Specification<Question> specification, Pageable pageable);
+
+    @Query("SELECT "
+            + "DISTINCT question "
+            + "FROM Question question "
+            + "LEFT OUTER JOIN Member authorQuestion ON question.author=authorQuestion "
+            + "LEFT OUTER JOIN Answer answer ON answer.question=question "
+            + "LEFT OUTER JOIN Member authorAnswer ON answer.author=authorAnswer "
+            + "WHERE "
+            + "   question.subject LIKE %:word% "
+            + "   OR question.content LIKE %:word% "
+            + "   OR authorQuestion.username LIKE %:word% "
+            + "   OR answer.content LIKE %:word% "
+            + "   OR authorAnswer.username LIKE %:word% ")
+    Page<Question> findAllByWord(@Param("word") String word, Pageable pageable);
 }
