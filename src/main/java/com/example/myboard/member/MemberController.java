2b3b61b10d83ec4fe2433a7702c258d61a4929bd
justlikeryu <justlikeryuu@gmail.com>
2024-05-06 22:58:54 +0900

    Feat: 불필요한 주석 삭제

--- src/main/java/com/example/myboard/member/MemberController.java
+++ src/main/java/com/example/myboard/member/MemberController.java
@@ -47,7 +47,7 @@ public class MemberController {
 
             return "signup_form";
         }
-        //todo redirect시 404 not found & 로그인 버튼 클릭 시에도 동일 에러 발생.
+
         return "redirect:/";
     }
 
--- src/main/java/com/example/myboard/question/QuestionForm.java
+++ src/main/java/com/example/myboard/question/QuestionForm.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QuestionForm {
-    //todo 아무 것도 입력하지 않으면 @NotEmpty에 의해 등록이 되지 않아야 하는데 등록이 되는 현상...
     @NotEmpty(message = "제목을 입력해 주세요.")//null, 빈 문자열 비허용
     @Size(max=200)//최대 길이는 200바이트
     private String subject;
